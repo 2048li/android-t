e15751305e69c15d20620bece6747e14afd0b8b2
shentanli <shentanli@shentanli-mac.ipads-lab.se.sjtu.edu.cn>
2016-03-05 16:43:13 +0800

    add an example

--- reflection/ReflectionTest/app/src/main/java/com/example/shentanli/reflectiontest/MainActivity.java
+++ reflection/ReflectionTest/app/src/main/java/com/example/shentanli/reflectiontest/MainActivity.java
@@ -9,23 +9,63 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+// reflect private obj in worker
+
 public class MainActivity extends AppCompatActivity {
+    private Field mStudentFiled ;
+    private Object mStudentObject;
+    private Class mStudentClass;
+    private Method mStudentShowMethod;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // setContentView(R.layout.activity_main);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       // fab.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+       //     public void onClick(View view) {
+       //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+       //                 .setAction("Action", null).show();
+       //     }
+      //  });
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        try{
+            String workerClassName = "com.example.shentanli.reflectiontest.Worker";
+            Class workerClass = Class.forName(workerClassName);
+            mStudentFiled = workerClass.getDeclaredField("mStudent");
+            mStudentFiled.setAccessible(true);
+            mStudentObject = mStudentFiled.get(new Worker());
+            mStudentClass = Class.forName(mStudentObject.getClass().getName());
+            mStudentShowMethod = mStudentClass.getDeclaredMethod("show");
+            mStudentShowMethod.setAccessible(true);
+            mStudentShowMethod.invoke(mStudentObject);
+
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
