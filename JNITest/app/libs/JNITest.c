502859ed7359ee12e797b34fd5a2a0df16e9643a
2048li <shentanli@qq.com>
2016-03-07 07:58:58 +0800

    MODIFY

--- JNITest/app/libs/JNITest.c
+++ JNITest/app/libs/JNITest.c
@@ -2,14 +2,14 @@
 // Created by shentanli on 3/5/16.
 //
 
-#include "NAME.h"
+#include "com_example_shentanli_jnitest_JNITest.h"
 
-#define LOG_TAG "JNITest"
-#undef LOG
+//#define LOG_TAG "JNITest"
+//#undef LOG
 
 #include <utils/log.h>
 
-JNIEXPORT jstring JNICALL Java_NAME_GetTest(JNIEnv * env, jobject obj)
+JNIEXPORT jstring JNICALL Java_com_example_shentanli_jnitest_JNITest_GetTest(JNIEnv * env, jobject obj)
 {
     return (*env)->NewStringUTF(env, (char *)"JNITest Native String");
     LOGD("Hello LIb!\n");
