1651cd90454d25eb240732c3376ba74cc1445f24
2048li <shentanli@qq.com>
2016-03-09 03:59:45 -0500

    refine

--- Crossapkactivity/Callee/app/src/main/java/com/example/shentanli/callee/Callee.java
+++ Crossapkactivity/Callee/app/src/main/java/com/example/shentanli/callee/Callee.java
@@ -20,7 +20,7 @@ public class Callee extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "this is the activity from callee", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
--- Crossapkactivity/Caller/app/src/main/java/com/example/shentanli/caller/Caller.java
+++ Crossapkactivity/Caller/app/src/main/java/com/example/shentanli/caller/Caller.java
@@ -20,20 +20,20 @@ public class Caller extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caller);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    //    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+     //   setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         try{
-            ComponentName componentname= new ComponentName("com.example.shentanli.callee","Callee");
+            ComponentName componentname= new ComponentName("com.example.shentanli.callee","com.example.shentanli.callee.Callee");
            /* System.out.println("the name of ......" + toActivity.getClassName().toString());*/
             Intent intent = new Intent();
             intent.setComponent(componentname);
@@ -42,6 +42,7 @@ public class Caller extends AppCompatActivity {
             startActivity(intent);
 
         }catch(Exception e){
+            Log.v("go to apk error","----"+e.toString());
             Toast.makeText(getApplicationContext(), "没找到程序", Toast.LENGTH_SHORT ).show();
             Log.v("go to apk error","----"+e.toString());
         }
