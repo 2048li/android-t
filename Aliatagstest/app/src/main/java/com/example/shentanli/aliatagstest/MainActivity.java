a7649c4f2a517d57081167df6e9eda33c03afd56
2048li <shentanli@qq.com>
2016-03-19 05:09:12 -0400

    processbuilder

--- Aliatagstest/app/src/main/java/com/example/shentanli/aliatagstest/MainActivity.java
+++ Aliatagstest/app/src/main/java/com/example/shentanli/aliatagstest/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.shentanli.aliatagstest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class MainActivity extends Activity {
@@ -12,7 +17,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        try {
+            //Process process = new ProcessBuilder().command("/system/bin/ping","android.com").redirectErrorStream(true).start();
+            Process process = new ProcessBuilder().command("/system/bin/sh su root").start();
+            InputStream in = process.getInputStream();
+            OutputStream out = process.getOutputStream();
+           // readStream(in);
+            Log.i("shentanli=====",in.toString());
+            Log.i("shentnali-----",out.toString());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+      //  finally{
+           // process.destroy();
+
+  //  }
+  }
 
 
     @Override
